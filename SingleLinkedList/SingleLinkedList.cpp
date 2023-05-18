dfda3c3c309300974a0a44321b620add398d3f47
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-18 19:46:26 +0700

    Mengimplementasikan pencarian posisi yang tepat dalam linked list untuk menyisipkan node baru

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -32,4 +32,16 @@ void addNode() {
 		return;
 	}
 
-	
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
