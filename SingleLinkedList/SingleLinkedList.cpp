8a5e9ece69d3dad16ddf1b0a8b1e91649d3a45e7
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-05-18 19:56:38 +0700

    Melengkapi implementasi dalam fungsi main() untuk pilihan 3 (Tampilkan Data), pilihan 4 (Cari Data), dan pilihan 5 (Keluar)

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -167,4 +167,19 @@ int main() {
 					cout << "nim: " << nim << " berhasil dihapus" << endl;
 					system("pause");
 					system("cls");
-				
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+		
